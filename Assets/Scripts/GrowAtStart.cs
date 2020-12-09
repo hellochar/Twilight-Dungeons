d31c62f6bc0cc1407fb4f73454cff7e842448508
Xiaohan Zhang <hellocharlien@hotmail.com>
2020-12-09 16:12:39 -0700

    grow at start animation for actors, plants, and grasses

---
+++ Assets/Scripts/GrowAtStart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowAtStart : MonoBehaviour {
+  private Vector3 initialScale;
+  // Start is called before the first frame update
+  void Start() {
+    initialScale = transform.localScale;
+    transform.localScale = transform.localScale * 0.01f;
+  }
+
+  // Update is called once per frame
+  void Update() {
+    if (Vector3.Distance(transform.localScale, initialScale) < 0.01f) {
+      transform.localScale = initialScale;
+      // we're done, remove ourselves
+      Destroy(this);
+    } else {
+      transform.localScale = Vector3.Lerp(transform.localScale, initialScale, 10f * Time.deltaTime);
+    }
+  }
+}

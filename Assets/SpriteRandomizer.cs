793536d6dd0032c5adcc621da0f9cf18aa814df4
Xiaohan Zhang <hellocharlien@hotmail.com>
2020-12-09 13:32:42 -0700

    sprite randomizer script

---
+++ Assets/SpriteRandomizer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRandomizer : MonoBehaviour {
+  public new SpriteRenderer renderer;
+  public Sprite[] sprites;
+  void Start() {
+    if (renderer == null) {
+      renderer = GetComponent<SpriteRenderer>();
+    }
+    renderer.sprite = Util.RandomPick(sprites);
+  }
+}

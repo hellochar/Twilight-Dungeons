f80428d61d2a77f1a8197d7e798eed13fd974b5c
Xiaohan Zhang <hellocharlien@hotmail.com>
2020-12-10 16:09:41 -0700

    EquipmentSlotController

--- Assets/src/controllers/EquipmentSlotController.cs
+++ Assets/src/controllers/EquipmentSlotController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MatchEquipmentSlot : MatchInventorySlot {
+public class EquipmentSlotController : MatchInventorySlot {
   public EquipmentSlot slot;
   public GameObject placeholder;
 

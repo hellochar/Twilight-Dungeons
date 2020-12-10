5e165b2b61e1aec19149e620d932f71f33aecb03
Xiaohan Zhang <hellocharlien@hotmail.com>
2020-12-10 15:57:36 -0700

    AttackGroundTask and RunAwayTask controllers

--- Assets/Scripts/AttackGroundTaskController.cs
+++ Assets/Scripts/AttackGroundTaskController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MatchAttackGroundTask : ActorTaskController<AttackGroundTask> {
+public class AttackGroundTaskController : ActorTaskController<AttackGroundTask> {
 
   public override void Start() {
     base.Start();
--- Assets/Scripts/RunAwayTaskController.cs
+++ Assets/Scripts/RunAwayTaskController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MatchRunAwayTask : ActorTaskController<RunAwayTask> {
+public class RunAwayTaskController : ActorTaskController<RunAwayTask> {
 
   public override void Start() {
     transform.localPosition = new Vector3(0, 0.5f, 0);

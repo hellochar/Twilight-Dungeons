8e9e7aca43a2241416b4792c4b68a56b33c31ded
Xiaohan Zhang <hellocharlien@hotmail.com>
2020-12-10 17:08:31 -0700

    InWorldEquipmentController; add a controllers/tiles/ folder

--- Assets/src/controllers/InWorldEquipmentController.cs
+++ Assets/src/controllers/InWorldEquipmentController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RenderEquipmentInWorld : ItemSlotController {
+public class InWorldEquipmentController : ItemSlotController {
   private GameObject itemPrefab;
 
   public EquipmentSlot slot;

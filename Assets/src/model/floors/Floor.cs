b3d446a4c43d5831e198e25cdf22c7031980e202
Xiaohan Zhang <hellocharlien@hotmail.com>
2020-12-29 16:51:38 -0700

    go back to dynamic pathfinding

--- Assets/src/model/floors/Floor.cs
+++ Assets/src/model/floors/Floor.cs
@@ -165,7 +165,8 @@ public class Floor {
   /// if pretendTargetEmpty is true, override the target tile to be walkable. You can then trim off
   /// the very end.
   internal List<Vector2Int> FindPath(Vector2Int pos, Vector2Int target, bool pretendTargetEmpty = false) {
-    return pathfindingManager.FindPath(pos, target);
+    // return pathfindingManager.FindPathStatic(pos, target);
+    return pathfindingManager.FindPathDynamic(pos, target, pretendTargetEmpty);
   }
 
   internal void CatchUpStep(float time) {
@@ -395,8 +396,36 @@ class PathfindingManager {
     }
   }
 
-  /// find path around tiles
-  public List<Vector2Int> FindPath(Vector2Int pos, Vector2Int target) {
+  public List<Vector2Int> FindPathDynamic(Vector2Int pos, Vector2Int target, bool pretendTargetEmpty) {
+    float[,] tilesmap = new float[floor.width, floor.height];
+    for (int x = 0; x < floor.width; x++) {
+      for (int y = 0; y < floor.height; y++) {
+        Tile tile = floor.tiles[x, y];
+        float weight = tile.GetPathfindingWeight();
+        tilesmap[x, y] = weight;
+      }
+    }
+    if (pretendTargetEmpty) {
+      tilesmap[target.x, target.y] = 1f;
+    }
+    // every float in the array represent the cost of passing the tile at that position.
+    // use 0.0f for blocking tiles.
+
+    // create a grid
+    PathFind.Grid grid = new PathFind.Grid(floor.width, floor.height, tilesmap);
+
+    // create source and target points
+    PathFind.Point _from = new PathFind.Point(pos.x, pos.y);
+    PathFind.Point _to = new PathFind.Point(target.x, target.y);
+
+    // get path
+    // path will either be a list of Points (x, y), or an empty list if no path is found.
+    List<PathFind.Point> path = PathFind.Pathfinding.FindPath(grid, _from, _to);
+    return path.Select(p => new Vector2Int(p.x, p.y)).ToList();
+  }
+
+  /// find path around tiles; don't take actor bodies into account
+  public List<Vector2Int> FindPathStatic(Vector2Int pos, Vector2Int target) {
     if (grid == null) {
       RecomputePathFindGrid();
     }

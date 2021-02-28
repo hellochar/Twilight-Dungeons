f4f243b70a656982034f551b2ac84412367d5e3d
Xiaohan Zhang <hellocharlien@hotmail.com>
2021-02-27 18:30:54 -0800

    flash damage border when you get hit at all

--- Assets/src/controllers/hud/DamageFlashController.cs
+++ Assets/src/controllers/hud/DamageFlashController.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 
-public class DamageFlashController : MonoBehaviour {
+public class DamageFlashController : MonoBehaviour, ITakeAnyDamageHandler {
   private Animator animator;
 
   void Start() {
     animator = GetComponent<Animator>();
+    GameModel.main.player.nonserializedModifiers.Add(this);
     Update();
+  }
+
+  public void HandleTakeAnyDamage(int damage) {
+    animator.SetTrigger("Hit");
   }
 
   // Update is called once per frame

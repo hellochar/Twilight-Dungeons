b3c090a9a3517557d880675b453332a3d7e6c41e
aelichung <aelichung@gmail.com>
2021-06-08 12:48:44 -0700

    add old dude vis change functionality

--- Assets/src/model/actors/OldDude.cs
+++ Assets/src/model/actors/OldDude.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Linq;
 using UnityEngine;
 
 [Serializable]
 [ObjectInfo(description: "An old dude. Looks like he's seen his way around the caves a few times.")]
 public class OldDude : AIActor {
+
   public OldDude(Vector2Int pos) : base(pos) {
     faction = Faction.Neutral;
     hp = baseMaxHp = 8;
@@ -12,5 +14,16 @@ public class OldDude : AIActor {
 
   protected override ActorTask GetNextTask() {
         return new MoveRandomlyTask(this);
+  }
+
+  public void RevealFloor(){
+    var f = this.floor;
+    var allPositions = f.EnumerateFloor();
+    foreach(Vector2Int p in allPositions){
+      Tile t = floor.tiles[p];
+      if(t.visibility == TileVisiblity.Unexplored){
+        t.visibility = TileVisiblity.Explored;
+      }
+    }
   }
 }

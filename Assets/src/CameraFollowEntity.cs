fbf539714fe3e0ae77f5ce6df8e15951ff0653df
Xiaohan Zhang <hellocharlien@hotmail.com>
2021-12-22 23:33:16 -0700

    experiment in moving camera to follow active entity (jank)

--- Assets/src/CameraFollowEntity.cs
+++ Assets/src/CameraFollowEntity.cs
@@ -10,6 +10,15 @@ public class CameraFollowEntity : MonoBehaviour {
 
   // Update is called once per frame
   void Update() {
+    // GameObject thisTarget = target;
+    // var activeEntity = GameModel.main.turnManager.activeEntity;
+    // if (activeEntity is Entity e) {
+    //   var gameObject = FloorController.current.GameObjectFor(e);
+    //   if (gameObject) {
+    //     thisTarget = gameObject;
+    //   }
+    // }
+
     // jump immediately if too far away
     if (Vector2.Distance(Util.getXY(this.transform.position), Util.getXY(this.target.transform.position)) > jumpThreshold) {
       this.transform.position = Util.withZ(Util.getXY(this.target.transform.position), this.transform.position.z);

24cc22026b2e841b404662a26d67f65728a8fabc
Xiaohan Zhang <hellocharlien@hotmail.com>
2022-07-16 17:17:01 -0500

    JackalHide now blocks 1 damage.

--- Assets/src/model/enemies/ItemJackalHide.cs
+++ Assets/src/model/enemies/ItemJackalHide.cs
@@ -1,13 +1,17 @@
 [System.Serializable]
 [ObjectInfo(spriteName: "jackal-fur", flavorText: "Patches of matted fur strewn together.")]
-public class ItemJackalHide : EquippableItem, IMaxHPModifier {
+public class ItemJackalHide : EquippableItem, IDurable, IAttackDamageTakenModifier {
   public override EquipmentSlot slot => EquipmentSlot.Armor;
+
+  public int durability { get; set; }
+
+  public int maxDurability => 4;
 
   public ItemJackalHide() {}
 
   public int Modify(int input) {
-    return input + 4;
+    return input - 1;
   }
 
-  internal override string GetStats() => "+4 max HP.";
+  internal override string GetStats() => "Blocks 1 damage.";
 }

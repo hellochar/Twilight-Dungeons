726825ae4e5ee10b9fa3fd342de7d5653ecb0548
Xiaohan Zhang <hellocharlien@hotmail.com>
2022-09-10 16:35:11 -0700

    Fungal Colony - make room 13x9

--- Assets/src/generator/FloorGenerator.cs
+++ Assets/src/generator/FloorGenerator.cs
@@ -493,35 +493,51 @@ public class FloorGenerator {
   }
 
   public Floor generateFungalColonyBossFloor(int depth) {
-    Floor floor = new BossFloor(depth, 27, 13);
+    Floor floor = new BossFloor(depth, 13, 9);
     foreach (var p in floor.EnumerateFloor()) {
       floor.Put(new Wall(p));
     }
 
     Room room0 = new Room(floor);
 
-    void CutOutCircle(Vector2Int center, float radius, bool addBoss = false) {
+    void CutOutCircle(Vector2Int center, float radius) {
       FloorUtils.CarveGround(floor, floor.EnumerateCircle(center, radius));
-      // connect it back to center
-      FloorUtils.CarveGround(floor, FloorUtils.Line3x3(floor, center, room0.center));
     }
 
     // start and end paths
-    CutOutCircle(new Vector2Int(4, floor.height / 2), 2.5f);
-    CutOutCircle(new Vector2Int(floor.width - 4, floor.height / 2), 2.5f);
-    CutOutCircle(room0.center, 5.5f);
+    CutOutCircle(new Vector2Int(3, floor.height / 2), 2f);
+    CutOutCircle(new Vector2Int(floor.width - 4, floor.height / 2), 2f);
+    CutOutCircle(room0.center, 4.5f);
     floor.Put(new FungalColony(room0.center));
 
-    // turn some of the walls into fungal walls
-    foreach (var pos in floor.tiles.Where(t => t is Wall && floor.GetAdjacentTiles(t.pos).Any(t2 => t2.CanBeOccupied())).Select(t => t.pos).ToList()) {
-      if (MyRandom.value < 0.25) {
+    // turn some of the walls and empty space into fungal walls
+    foreach (var pos in floor.EnumerateFloor()) {
+      var t = floor.tiles[pos];
+      if (t is Wall && floor.GetAdjacentTiles(t.pos).Any(t2 => t2.CanBeOccupied())) {
         floor.Put(new FungalWall(pos));
+      } else if (t is Ground && t.CanBeOccupied()) {
+        if (MyRandom.value < 0.25f) {
+          floor.Put(new FungalWall(pos));
+        }
       }
     }
+
+    // re-apply normal Wall to outer perimeter
+    floor.PutAll(floor.EnumeratePerimeter().Select(pos => new Wall(pos)));
+
     // block entrance
-    floor.PutAll(new FungalWall(new Vector2Int(7, 5)), new FungalWall(new Vector2Int(7, 6)), new FungalWall(new Vector2Int(7, 7)));
-    floor.PlaceUpstairs(new Vector2Int(2, floor.height / 2));
-    floor.PlaceDownstairs(new Vector2Int(floor.width - 2, floor.height / 2));
+    // floor.PutAll(new FungalWall(new Vector2Int(7, 5)), new FungalWall(new Vector2Int(7, 6)), new FungalWall(new Vector2Int(7, 7)));
+    floor.PlaceUpstairs(new Vector2Int(0, floor.height / 2));
+    floor.PlaceDownstairs(new Vector2Int(floor.width - 1, floor.height / 2));
+
+    floor.root = room0;
+    floor.rooms = new List<Room>();
+    floor.upstairsRoom = room0;
+    floor.downstairsRoom = room0;
+
+    FloorUtils.TidyUpAroundStairs(floor);
+    return floor;
+  }
 
     floor.root = room0;
     floor.rooms = new List<Room>();

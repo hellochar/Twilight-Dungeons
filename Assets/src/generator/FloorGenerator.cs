01eed8fc2d2019e50a1cf1d3dd2fa82a5edbec2a
Xiaohan Zhang <hellocharlien@hotmail.com>
2022-09-10 16:32:42 -0700

    Sculpting progression
    Remove extra grass spawn from all rooms (Good modifier to add in later though)
    
    Early game:
    
    * Early game floors are now 9x7 - just a bit more space to hopefully show some more creature behaviors
    * Spawn only 1 grass on depths 4-8
    
    Midgame:
    
    * Early midgame only has 2 enemies/1 grass to more gently introduce new biome

--- Assets/src/generator/FloorGenerator.cs
+++ Assets/src/generator/FloorGenerator.cs
@@ -35,48 +35,47 @@ public class FloorGenerator {
 
   private void InitFloorGenerators() {
     floorGenerators = new List<Func<Floor>>() {
+      // early game
       () => generateFloor0(0),
-      () => generateSingleRoomFloor(1, 9, 6),
-      () => generateSingleRoomFloor(2, 9, 6, extraEncounters: Encounters.OneAstoria),
-      () => generateSingleRoomFloor(3, 9, 6),
-      () => generateSingleRoomFloor(4, 9, 6, 2, 1, true, extraEncounters: Encounters.OneAstoria),
-      () => generateSingleRoomFloor(5, 9, 6, 2),
-      () => generateSingleRoomFloor(6, 9, 6, 2),
-      () => generateSingleRoomFloor(7, 9, 6, 2),
+      () => generateSingleRoomFloor(1, 9, 7, 1, 1),
+      () => generateSingleRoomFloor(2, 9, 7, 1, 1, extraEncounters: Encounters.OneAstoria),
+      () => generateSingleRoomFloor(3, 9, 7, 1, 1),
+      () => generateSingleRoomFloor(4, 9, 7, 2, 1, true, extraEncounters: Encounters.OneAstoria),
+      () => generateSingleRoomFloor(5, 9, 7, 2, 1),
+      () => generateSingleRoomFloor(6, 9, 7, 2, 1),
+      () => generateSingleRoomFloor(7, 9, 7, 2, 1),
       () => generateRewardFloor(8, shared.Plants.GetRandomAndDiscount(1f), Encounters.OneAstoria),
       () => generateSingleRoomFloor(9, 10, 7, 3, 2),
       () => generateSingleRoomFloor(10, 10, 7, 3, 2),
       () => generateSingleRoomFloor(11, 10, 7, 3, 2, true, null, Encounters.AddDownstairsInRoomCenter),
       () => generateBlobBossFloor(12),
-      () => generateSingleRoomFloor(13, 11, 8, 4, 3),
-      () => generateSingleRoomFloor(14, 11, 8, 4, 3),
-      () => generateSingleRoomFloor(15, 11, 8, 5, 3),
+
+      // midgame
+      () => generateSingleRoomFloor(13, 11, 8, 2, 1),
+      () => generateSingleRoomFloor(14, 11, 8, 2, 1),
+      () => generateSingleRoomFloor(15, 11, 8, 2, 1),
       () => generateRewardFloor(16, shared.Plants.GetRandomAndDiscount(1f), Encounters.OneAstoria),
-      () => generateSingleRoomFloor(17, 12, 8, 4, 2),
-      () => generateSingleRoomFloor(18, 12, 8, 4, 2),
-      () => generateSingleRoomFloor(19, 12, 8, 5, 3, true),
+      () => generateSingleRoomFloor(17, 12, 8, 3, 2),
+      () => generateSingleRoomFloor(18, 12, 8, 3, 2),
+      () => generateSingleRoomFloor(19, 12, 8, 4, 3, true),
       () => generateSingleRoomFloor(20, 12, 8, 5, 2),
       () => generateSingleRoomFloor(21, 12, 8, 6, 3),
       () => generateSingleRoomFloor(22, 12, 8, 7, 4, true, null, Encounters.AddDownstairsInRoomCenter, Encounters.FungalColonyAnticipation),
       () => generateFungalColonyBossFloor(23),
       () => generateRewardFloor(24, shared.Plants.GetRandomAndDiscount(1f), Encounters.AddWater),
+
+      // endgame
       () => generateSingleRoomFloor(25, 12, 8, 2, 2),
-      () => generateSingleRoomFloor(26, 12, 8, 3, 3),
-      () => generateSingleRoomFloor(27, 13, 9, 4, 3, false, null, Encounters.AddWater),
-      () => generateSingleRoomFloor(28, 13, 9, 5, 3, false, new Encounter[] { Encounters.LineWithOpening, Encounters.ChasmsAwayFromWalls2 }),
-      () => generateSingleRoomFloor(29, 13, 9, 5, 2),
+      () => generateSingleRoomFloor(26, 12, 8, 2, 2),
+      () => generateSingleRoomFloor(27, 13, 9, 3, 3, false, null, Encounters.AddWater),
+      () => generateSingleRoomFloor(28, 13, 9, 4, 3, false, new Encounter[] { Encounters.LineWithOpening, Encounters.ChasmsAwayFromWalls2 }),
+      () => generateSingleRoomFloor(29, 13, 9, 5, 3),
       () => generateSingleRoomFloor(30, 13, 9, 6, 3),
       () => generateSingleRoomFloor(31, 14, 9, 7, 4),
-      // () => generateMultiRoomFloor(29, 24, 20, 10),
-      // () => generateMultiRoomFloor(30, 24, 24, 12, true),
-      // () => generateMultiRoomFloor(31, 24, 16, 12),
       () => generateRewardFloor(32, shared.Plants.GetRandomAndDiscount(1f), Encounters.AddWater, Encounters.ThreeAstoriasInCorner),
       () => generateSingleRoomFloor(33, 14, 9, 8, 5),
       () => generateSingleRoomFloor(34, 14, 9, 9, 6),
       () => generateSingleRoomFloor(35, 14, 9, 10, 7),
-      // () => generateMultiRoomFloor(33, 26, 17, 15, true),
-      // () => generateMultiRoomFloor(34, 28, 19, 20),
-      // () => generateMultiRoomFloor(35, 45, 25, 30),
       () => generateEndFloor(36),
     };
   }
@@ -389,10 +388,12 @@ public class FloorGenerator {
   /// Generates one single room with one wall variation, X mob encounters, Y grass encounters, an optional reward.
   /// Good for a contained experience.
   /// </summary>
-  public Floor generateSingleRoomFloor(int depth, int width, int height, int numMobs = 1, int numGrasses = 1, bool reward = false, Encounter[] preMobEncounters = null, params Encounter[] extraEncounters) {
-    numGrasses++;
+  public Floor generateSingleRoomFloor(int depth, int width, int height, int numMobs, int numGrasses, bool reward = false, Encounter[] preMobEncounters = null, params Encounter[] extraEncounters) {
     Floor floor = tryGenerateSingleRoomFloor(depth, width, height, preMobEncounters == null);
     ensureConnectedness(floor);
+    floor.PutAll(
+      floor.EnumeratePerimeter().Where(pos => floor.tiles[pos] is Ground).Select(pos => new Wall(pos))
+    );
     var room0 = floor.root;
     if (preMobEncounters != null) {
       foreach (var encounter in preMobEncounters) {

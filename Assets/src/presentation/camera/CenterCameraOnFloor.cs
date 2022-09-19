696d9759c98f6bff28ff8cf838b33f3da186bf65
Xiaohan Zhang <hellocharlien@hotmail.com>
2022-09-18 17:58:14 -0700

    minZoom parameter on CenterCameraOnFloor

--- Assets/src/presentation/camera/CenterCameraOnFloor.cs
+++ Assets/src/presentation/camera/CenterCameraOnFloor.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 /// disallow camera from seeing "outside" the edge of the map
 public class CenterCameraOnFloor : MonoBehaviour {
   public float paddingTop = 2, paddingRight = 2, paddingBottom = 2, paddingLeft = 2;
+  public float minZoom = 0;
   new private Camera camera;
 
   // Start is called before the first frame update
@@ -19,6 +20,6 @@ public class CenterCameraOnFloor : MonoBehaviour {
     bounds.max = Util.withZ(GameModel.main.currentFloor.boundsMax) + new Vector3(paddingRight, paddingTop, 0);
 
     this.transform.position = new Vector3(bounds.center.x, bounds.center.y - 0.5f, this.transform.position.z);
-    camera.orthographicSize = bounds.extents.y;
+    camera.orthographicSize = Mathf.Max(minZoom, bounds.extents.y);
   }
 }

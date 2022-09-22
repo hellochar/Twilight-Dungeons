83436d4c23ca6e5c9d0957eab70f919cabc70731
Xiaohan Zhang <hellocharlien@hotmail.com>
2022-09-21 23:19:39 -0700

    New Destructible Slime that gives you water and leaves a Slimy Status on you

---
+++ Assets/src/model/actors/Slime.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+[ObjectInfo(description: "Blocks your path. Leaves a debuff when you kill it")]
+public class Slime : Destructible, IDeathHandler {
+  public Slime(Vector2Int pos) : base(pos, 3) {
+  }
+
+  public void HandleDeath(Entity source) {
+    GameModel.main.player.water += 60;
+    GameModel.main.player.statuses.Add(new SlimyStatus(1, GameModel.main.currentFloor.depth));
+  }
+}
+
+[System.Serializable]
+[ObjectInfo("slimed", "You're filled with sticky slime! You take more damage.")]
+internal class SlimyStatus : StackingStatus, IAttackDamageTakenModifier {
+  public override bool isDebuff => true;
+  public override StackingMode stackingMode => StackingMode.Add;
+  public int depth;
+  public SlimyStatus(int stacks, int depth) : base(stacks) {
+    this.depth = depth;
+  }
+
+  public override string Info() => $"You take {stacks} more attack damage.";
+
+  public override void Step() {
+    if (GameModel.main.currentFloor.depth != depth) {
+      Remove();
+    }
+  }
+
+  public int Modify(int input) {
+    return input + stacks;
+  }
+}

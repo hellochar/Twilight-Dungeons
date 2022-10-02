27805e817fd107ce942b63084ed3a62867738738
Xiaohan Zhang <hellocharlien@hotmail.com>
2022-10-01 20:02:52 -0700

    show popup on BodyControllrt

--- Assets/src/controllers/actors/BodyController.cs
+++ Assets/src/controllers/actors/BodyController.cs
@@ -46,6 +46,9 @@ public class BodyController : MonoBehaviour, IEntityController, IPlayerInteractH
     if (body.IsDead) {
       return null; // don't do anything to dead actors
     }
+    if (body.floor.depth == 0) {
+      return new ArbitraryPlayerInteraction(() => InteractionController.ShowPopupFor(body));
+    }
     return new SetTasksPlayerInteraction(
       new ChaseTargetTask(player, body),
       new AttackTask(player, body)

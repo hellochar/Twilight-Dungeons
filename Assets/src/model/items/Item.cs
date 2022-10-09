fe066b285e213010fd7e17df673e0850d0b79c23
Xiaohan Zhang <hellocharlien@hotmail.com>
2022-10-08 21:38:11 -0700

    Disable Item Destroy method
    You can just drop them for Mulch at the home floor

--- Assets/src/model/items/Item.cs
+++ Assets/src/model/items/Item.cs
@@ -38,7 +38,7 @@ public class Item : IModifierProvider {
 
   public virtual List<MethodInfo> GetAvailableMethods(Player player) {
     var methods = new List<MethodInfo>() {
-      GetType().GetMethod("Destroy"),
+      // GetType().GetMethod("Destroy"),
       GetType().GetMethod("Drop")
     };
     if (this is IEdible edible) {

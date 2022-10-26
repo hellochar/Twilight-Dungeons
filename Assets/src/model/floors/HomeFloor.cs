3458d27b884a438a8db53c64eb9c1caae6ff0168
Xiaohan Zhang <hellocharlien@hotmail.com>
2022-10-25 22:34:49 -0700

    Only get one organic matter per Cut

--- Assets/src/model/floors/HomeFloor.cs
+++ Assets/src/model/floors/HomeFloor.cs
@@ -63,7 +63,6 @@ public class ThickBrush : Destructible, IBlocksVision {
       f.root.ExtendToEncompass(new Room(pos - Vector2Int.one, pos + Vector2Int.one));
       f.RecomputeVisibility();
     }
-    floor.Put(new ItemOnGround(pos, new ItemOrganicMatter()));
   }
 
   [PlayerAction]
@@ -71,18 +70,16 @@ public class ThickBrush : Destructible, IBlocksVision {
     // cut this 3x3 area
     var player = GameModel.main.player;
     player.UseActionPointOrThrow();
-    var room = this.room;
-    if (room != null) {
-      // var xStart = (pos.x / 3) * 3;
-      // var yStart = (pos.y / 3) * 3;
-      // foreach(var p in floor.EnumerateRectangle(new Vector2Int(xStart, yStart), new Vector2Int(xStart + 3, yStart + 3))) {
-      foreach (var p in floor.GetAdjacentTiles(pos).Select(t => t.pos)) {
-        var brush = player.floor.bodies[p] as ThickBrush;
-        if (brush != null) {
-          brush.Kill(player);
-        }
+    // var xStart = (pos.x / 3) * 3;
+    // var yStart = (pos.y / 3) * 3;
+    // foreach(var p in floor.EnumerateRectangle(new Vector2Int(xStart, yStart), new Vector2Int(xStart + 3, yStart + 3))) {
+    foreach (var p in floor.GetAdjacentTiles(pos).Select(t => t.pos)) {
+      var brush = player.floor.bodies[p] as ThickBrush;
+      if (brush != null) {
+        brush.Kill(player);
       }
     }
+    player.floor.Put(new ItemOnGround(pos, new ItemOrganicMatter()));
     player.floor.RecomputeVisibility();
   }
 }

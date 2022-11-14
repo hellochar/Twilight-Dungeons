30458a191758616072bbce439c526b7d46146844
Xiaohan Zhang <hellocharlien@hotmail.com>
2022-11-13 16:31:42 -0800

    CaptureAction - get one Keep when clearing a room

--- Assets/src/model/floors/Floor.cs
+++ Assets/src/model/floors/Floor.cs
@@ -249,8 +249,36 @@ public class Floor {
           freeSpot = GetAdjacentTiles(GameModel.main.player.pos)[0];
         }
         Put(new Teleporter(freeSpot.pos));
+        new CaptureAction().ShowTargetingUIThenPerform(GameModel.main.player);
       }
     });
+  }
+
+  class CaptureAction : ITargetedAction<Entity> {
+    public string TargettedActionName => "Keep";
+    public string TargettedActionDescription => "Keep a Grass or Creature!";
+
+    public void PerformTargettedAction(Player player, Entity target) {
+      Item item = null;
+      if (target is Grass grass) {
+        item = new ItemGrass(grass.GetType());
+      } else if (target is AIActor actor) {
+        item = new ItemPlaceableEntity(target);
+      }
+
+      if (item != null) {
+        var success = player.inventory.AddItem(item, target);
+        if (!success) {
+          player.floor.Put(new ItemOnGround(target.pos, item));
+        }
+        // they're *not* killed because we don't want to trigger actions on them
+        target.floor.Remove(target);
+      }
+    }
+
+    public IEnumerable<Entity> Targets(Player player) {
+      return player.floor.grasses.Cast<Entity>().Concat(player.floor.bodies.OfType<AIActor>());
+    }
   }
 
   internal void PutAll(IEnumerable<Entity> entities) {

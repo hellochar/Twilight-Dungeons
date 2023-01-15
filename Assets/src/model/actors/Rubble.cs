452961be44fffa25b737b2432fe5d74197d8e13a
Xiaohan Zhang <hellocharlien@hotmail.com>
2023-01-15 13:21:13 -0800

    Stalks now all disappear when you kill one

--- Assets/src/model/actors/Rubble.cs
+++ Assets/src/model/actors/Rubble.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using UnityEngine;
 
 /// signals that attacking this doesn't use item durability
@@ -29,9 +30,20 @@ public class Stump : Destructible, IBlocksVision {
 
 [System.Serializable]
 [ObjectInfo(description: "Blocks vision.")]
-public class Stalk : Destructible, IBlocksVision {
+public class Stalk : Destructible, IBlocksVision, IDeathHandler {
 
   public Stalk(Vector2Int pos) : base(pos) {}
+
+  // kill all stalks on the map
+  public void HandleDeath(Entity source) {
+    if (!(source is Stalk)) {
+      foreach(var stalk in floor.bodies.OfType<Stalk>().ToArray()) {
+        if (stalk != this) {
+          stalk.Kill(this);
+        }
+      }
+    }
+  }
 }
 
 /// Note - not implemented on moving entities yet

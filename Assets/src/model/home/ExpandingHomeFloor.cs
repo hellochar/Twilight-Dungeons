e23aaa78e8c48e66ff60d873c3a59ba9cb6f6701
Xiaohan Zhang <hellocharlien@hotmail.com>
2023-01-15 02:20:45 -0800

    Introduce HomeSections
    * Rather than placing individual tiles, you get 5 and Merge into a random HomeSection
    * A HomeSection is a pre-built chunk of Tiles. Goal is to prevent players from doing the optimal layout of one long line

--- Assets/src/model/home/ExpandingHomeFloor.cs
+++ Assets/src/model/home/ExpandingHomeFloor.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 [Serializable]
@@ -54,4 +56,134 @@ public class ExpandingHomeFloor : HomeFloor {
       // f.RecomputeVisibility();
     }
   }
+}
+
+[Serializable]
+public class HomeSection {
+  public Type[,] Types { get; }
+  public readonly string source;
+  public HomeSection(Type[,] types, string source) {
+    Types = types;
+    this.source = source;
+  }
+
+  public int width => Types.GetLength(0);
+  public int height => Types.GetLength(1);
+
+  public void Blit(Floor floor, Vector2Int topLeft) {
+    var bottomLeft = topLeft - new Vector2Int(0, height - 1);
+    for (int x = 0; x < width; x++) {
+      for (int y = 0; y < height; y++) {
+        var pos = bottomLeft + new Vector2Int(x, y);
+        if (!floor.InBounds(pos)) {
+          continue;
+        }
+
+        var tileType = Types[x, y];
+        var constructor = tileType.GetConstructor(new Type[] { typeof(Vector2Int) });
+        var tile = (Tile)constructor.Invoke(new object[] { pos });
+        floor.Put(tile);
+      }
+    }
+  }
+
+  public static HomeSection FromString(string source) {
+    /* s will look like: @"
+xx_
+xxx
+ x
+"
+    */
+    source = source.Trim();
+    if (source.Length == 0) {
+      throw new Exception("Empty string!");
+    }
+    var lines = source.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+    // Text's coordinate system is +y down. Our game's coordinate system is +y up.
+    // To account for this we flip the order of lines here.
+    lines = lines.Reverse().ToArray();
+
+    var width = lines.Select(line => line.Length).Max();
+    var height = lines.Length;
+    Type[,] Types = new Type[width, height];
+    for(int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+        var line = lines[y];
+        var curChar = line[x];
+        Type tileType = null;
+        switch (curChar) {
+          case 'x':
+            tileType = typeof(HomeGround);
+            break;
+          case '0':
+            tileType = typeof(Wall);
+            break;
+          case '_':
+          case ' ':
+          default:
+            tileType = typeof(Chasm);
+            break;
+        }
+        Types[x, y] = tileType;
+      }
+    }
+    return new HomeSection(Types, source);
+  }
+
+  public static HomeSection[] FromMultiString(string multiSections) {
+    Regex r = new Regex("^$", RegexOptions.Multiline);
+    var sections2 = r.Split(multiSections);
+    var sections = multiSections.Split("====".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+    return sections.Select(section => FromString(section)).ToArray();
+  }
+
+  public static HomeSection[] StandardSections = FromMultiString(@"
+xx_
+xxx
+_xx
+
+====
+
+xx
+_x
+
+====
+
+_x_
+xxx
+_x_
+
+====
+
+_x_
+xxx
+__x
+
+====
+
+__x_
+xxxx
+
+====
+
+xxxx
+_x__
+
+====
+
+xxx
+xx_
+
+====
+
+xx_
+xxx
+
+====
+
+x__
+xxx
+x__
+");
 }
--- Assets/src/model/items/content/ItemPlaceableTile.cs
+++ Assets/src/model/items/content/ItemPlaceableTile.cs
@@ -19,15 +19,53 @@ public class ItemPlaceableTile : Item, ITargetedAction<Tile> {
     return (other as ItemPlaceableTile).tileType == tileType;
   }
 
+  [PlayerAction]
+  public void Merge() {
+    if (stacks >= 5) {
+      HomeSection section = Util.RandomPick(HomeSection.StandardSections);
+      ItemHomeSection item = new ItemHomeSection(section);
+      if (!inventory.AddItem(item)) {
+        GameModel.main.player.floor.Put(new ItemOnGround(GameModel.main.player.pos, item));
+      }
+      stacks -= 5;
+    }
+  }
+
   public void PerformTargettedAction(Player player, Entity target) {
-    var tile = (Tile) tileType
-      .GetConstructor(new Type[] { typeof(Vector2Int) })
-      .Invoke(new object[] { target.pos });
-    target.floor.Put(tile);
-    stacks--;
+    // var tile = (Tile) tileType
+    //   .GetConstructor(new Type[] { typeof(Vector2Int) })
+    //   .Invoke(new object[] { target.pos });
+    // target.floor.Put(tile);
+    // stacks--;
   }
 
   public IEnumerable<Tile> Targets(Player player) {
     return player.GetVisibleTiles().Where(t => t.GetType() != tileType);
+  }
+}
+
+[Serializable]
+public class ItemHomeSection : Item, ITargetedAction<Tile> {
+  public string TargettedActionName => "Place";
+  public string TargettedActionDescription => $"Place the Home Section.";
+  public HomeSection section;
+
+  public ItemHomeSection(HomeSection section) {
+    this.section = section;
+  }
+
+  public override string displayName => $"Home Section {Array.IndexOf(HomeSection.StandardSections, section) + 1}";
+
+  internal override string GetStats() {
+    return section.source;
+  }
+
+  public void PerformTargettedAction(Player player, Entity target) {
+    section.Blit(target.floor, target.pos);
+    Destroy();
+  }
+
+  public IEnumerable<Tile> Targets(Player player) {
+    return player.GetVisibleTiles();
   }
 }

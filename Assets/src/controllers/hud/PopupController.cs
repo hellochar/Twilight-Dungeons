ac8b1959b094c437de80b47f054125801dea0de3
Xiaohan Zhang <hellocharlien@hotmail.com>
2023-01-27 20:59:38 -0800

    WIP clickable item links in Item stats

--- Assets/src/controllers/hud/PopupController.cs
+++ Assets/src/controllers/hud/PopupController.cs
@@ -1,14 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class PopupController : MonoBehaviour, ICameraOverride {
+public class PopupController : MonoBehaviour, ICameraOverride, IPointerClickHandler {
   // set externally
   public Entity target;
   public event Action OnClose;
   internal bool showPlayerInventoryOnTop;
+
+  // Popups.cs line 30 sets this
+  public GameObject content => container.transform.Find("Content").gameObject;
 
   public Transform container;
   public GameObject overlay, overlayLeanLeft, overlayLeanRight;
@@ -80,6 +85,20 @@ public class PopupController : MonoBehaviour, ICameraOverride {
       var playerInventory = GameObject.Find("Inventory Container");
       playerInventory.transform.SetParent(playerInventoryOriginalParent);
       playerInventory.transform.SetSiblingIndex(playerInventoryOriginalSiblingIndex);
+    }
+  }
+
+  public void OnPointerClick(PointerEventData eventData) {
+    var infoText = container.transform.Find("Content/Stats")?.GetComponent<TMPro.TMP_Text>();
+		var linkIndex = TMP_TextUtilities.FindIntersectingLink(infoText, Input.mousePosition, null);
+    if (linkIndex != -1) {
+      var linkId = infoText.textInfo.linkInfo[linkIndex].GetLinkID();
+
+      if (linkId == "test") {
+        var snake = target as Snake;
+        var homeItem = snake.GetHomeItem();
+        ItemController.ShowItemPopup(homeItem, null);
+      }
     }
   }
 }

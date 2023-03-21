70710917f7a38912388a0d11ff467ed53e54e087
Xiaohan Zhang <hellocharlien@hotmail.com>
2023-03-21 02:03:35 -0700

    Rubble will sometimes turn into an ItemRubble that you can move around

--- Assets/src/model/actors/Rubble.cs
+++ Assets/src/model/actors/Rubble.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -18,8 +19,43 @@ public class Destructible : Body, IAnyDamageTakenModifier, IHideInSidebar {
 
 [Serializable]
 [ObjectInfo(description: "Destructible. Blocks vision.")]
-public class Rubble : Destructible, IBlocksVision {
+public class Rubble : Destructible, IBlocksVision, IDeathHandler {
   public Rubble(Vector2Int pos) : base(pos, 1) {}
+
+  public void HandleDeath(Entity source) {
+    if (MyRandom.value < 0.5f) {
+      var floor = this.floor;
+      GameModel.main.EnqueueEvent(() => {
+        floor.Put(new ItemOnGround(pos, new ItemRubble()));
+      });
+    }
+  }
+}
+
+[Serializable]
+[ObjectInfo("colored_transparent_packed_100")]
+internal class ItemRubble : Item, ITargetedAction<Ground> {
+  public string TargettedActionName => "Place";
+
+  public string TargettedActionDescription => "Choose where to place the Rubble.";
+
+  public void PerformTargettedAction(Player player, Entity target) {
+    player.SetTasks(
+      new MoveNextToTargetTask(player, target.pos),
+      new GenericOneArgTask<Ground>(player, PlaceRubble, target as Ground)
+    );
+  }
+
+  private void PlaceRubble(Ground ground) {
+    if (ground.CanBeOccupied()) {
+      ground.floor.Put(new Rubble(ground.pos));
+      stacks--;
+    } else {
+      throw new CannotPerformActionException("Tile occupied!");
+    }
+  }
+
+  public IEnumerable<Ground> Targets(Player player) => player.floor.tiles.Where(t => t.isVisible && t.CanBeOccupied()).OfType<Ground>();
 }
 
 [System.Serializable]

2ed43cf52674393dc2d603468ec0c234fb7e39fb
Xiaohan Zhang <hellocharlien@hotmail.com>
2023-03-21 23:13:32 -0700

    GrowingEntity - propagate shape

--- Assets/src/model/stations/GrowingEntity.cs
+++ Assets/src/model/stations/GrowingEntity.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 // A Body that, after one day, turns into its inner Entity
 [Serializable]
 public class GrowingEntity : Body, IDaySteppable {
+  public override Vector2Int[] shape => inner.shape;
   public Entity inner { get; private set; }
   public override string displayName => $"Growable {inner.displayName}";
   public override string description => $"Grows into a {inner.displayName} the next day.\n\n{inner.description}";

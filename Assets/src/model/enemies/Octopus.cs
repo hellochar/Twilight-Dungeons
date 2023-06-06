f9b5a366eb374e6e511081a88382626cdee6f842
Xiaohan Zhang <hellocharlien@hotmail.com>
2023-06-05 20:58:29 -0700

    Fix Octopus AI and make it only attack at range 2
    Add commented out ItemOctopusStinger

--- Assets/src/model/enemies/Octopus.cs
+++ Assets/src/model/enemies/Octopus.cs
@@ -1,7 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [System.Serializable]
-[ObjectInfo(description: "Attacks at range.\nRuns away if you get too close.")]
+[ObjectInfo(description: "Attacks at range 2.\nRuns away if you get too close.")]
 public class Octopus : AIActor {
   public override float turnPriority => task is AttackGroundTask ? 90 : base.turnPriority;
 
@@ -10,16 +13,22 @@ public class Octopus : AIActor {
     hp = baseMaxHp = 3;
   }
 
+  public static bool IsInRange(Entity octopus, Entity target) {
+    return Util.DiamondMagnitude(target.pos - octopus.pos) <= 2;
+  }
+
   protected override ActorTask GetNextTask() {
     var player = GameModel.main.player;
     if (IsNextTo(player)) {
       return new RunAwayTask(this, player.pos, 1, true);
     }
     if (CanTargetPlayer()) {
-      if (Util.DiamondDistanceToPlayer(this) < 3) {
-        return new AttackGroundTask(this, GameModel.main.player.pos, 1);
+      if (IsInRange(this, player)) {
+        return new AttackGroundTask(this, player.pos, 1);
       } else {
-        return new ChaseTargetTask(this, player, 3);
+        var chase = new ChaseTargetTask(this, player);
+        chase.maxMoves = 1;
+        return chase;
       }
     } else {
       return new WaitTask(this, 1);
@@ -28,3 +37,25 @@ public class Octopus : AIActor {
 
   internal override (int, int) BaseAttackDamage() => (1, 2);
 }
+
+// [Serializable]
+// [ObjectInfo("octopus-stinger", description: "Can attack at range 2.")]
+// internal class ItemOctopusStinger : EquippableItem, IWeapon, ITargetedAction<Body> {
+//   public override EquipmentSlot slot => EquipmentSlot.Weapon;
+//   public override int stacksMax => int.MaxValue;
+
+//   public (int, int) AttackSpread => (1, 2);
+
+//   string ITargetedAction<Body>.TargettedActionName => "Attack";
+
+//   string ITargetedAction<Body>.TargettedActionDescription => "Choose a target.";
+
+//   void ITargetedAction<Body>.PerformTargettedAction(Player player, Entity target) {
+//     player.SetTasks(new AttackGroundTask(player, target.pos, 0));
+//     // player.SetTasks(new AttackTask(player, target as Body));
+//   }
+
+//   IEnumerable<Body> ITargetedAction<Body>.Targets(Player player) {
+//     return player.floor.bodies.Where(b => Octopus.IsInRange(player, b));
+//   }
+// }

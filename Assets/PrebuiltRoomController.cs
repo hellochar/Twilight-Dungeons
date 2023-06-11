a4fc8df25c0ea9142400ad0797c89257b815e55f
Xiaohan Zhang <hellocharlien@hotmail.com>
2023-06-10 17:32:49 -0700

    Prebuilt for new tutorial room 1

---
+++ Assets/PrebuiltRoomController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PrebuiltRoomController : MonoBehaviour {
+
+  void Awake() {
+    // scan through all objects in the scene and convert them to gameobjects
+    Prebuilt prebuilt = Prebuilt.ConvertSceneIntoPrebuilt(SceneManager.GetActiveScene());
+
+    // immediately transition to a GameModel that just contains this floor and start the Game scene
+    GameModel.GenerateFromPrebuiltAndSetMain(prebuilt);
+    SceneManager.LoadScene("Scenes/Game");
+  }
+}

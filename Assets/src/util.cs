088056d2b8c6d611f80e11f3738ba5d80b45057d
Xiaohan Zhang <hellocharlien@hotmail.com>
2023-07-16 17:56:01 -0700

    Add WaitWhile, WaitUntil, and WaitUntilNotNull async methods

--- Assets/src/util.cs
+++ Assets/src/util.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public static class Util {
@@ -154,9 +155,49 @@ public static class Util {
     Math.Abs(v.x),
     Math.Abs(v.y)
   );
-  
+
   public static float DiamondMagnitude(Vector2 v) => Math.Max(
     Math.Abs(v.x),
     Math.Abs(v.y)
   );
+
+  /// <summary>
+  /// Blocks while condition is true or timeout occurs.
+  /// </summary>
+  /// <param name="condition">The condition that will perpetuate the block.</param>
+  /// <param name="frequency">The frequency at which the condition will be check, in milliseconds.</param>
+  /// <param name="timeout">Timeout in milliseconds.</param>
+  /// <exception cref="TimeoutException"></exception>
+  /// <returns></returns>
+  public static async Task WaitWhile(Func<bool> condition, int frequency = 100, int timeout = -1) {
+    var waitTask = Task.Run(async () => {
+      while (condition()) await Task.Delay(frequency);
+    });
+
+    if (waitTask != await Task.WhenAny(waitTask, Task.Delay(timeout))) {
+      throw new TimeoutException();
+    }
+  }
+
+  /// <summary>
+  /// Blocks until condition is true or timeout occurs.
+  /// </summary>
+  /// <param name="condition">The break condition.</param>
+  /// <param name="frequency">The frequency at which the condition will be checked.</param>
+  /// <param name="timeout">The timeout in milliseconds.</param>
+  /// <returns></returns>
+  public static async Task WaitUntil(Func<bool> condition, int frequency = 100, int timeout = -1) {
+    var waitTask = Task.Run(async () => {
+      while (!condition()) await Task.Delay(frequency);
+    });
+
+    if (waitTask != await Task.WhenAny(waitTask, Task.Delay(timeout))) {
+      throw new TimeoutException();
+    }
+  }
+
+  public static async Task<T> WaitUntilNotNull<T>(Func<T> getter) {
+    await WaitUntil(() => getter() != null);
+    return getter();
+  }
 }

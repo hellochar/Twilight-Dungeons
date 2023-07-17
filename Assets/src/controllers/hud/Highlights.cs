f90ff896276fc5a93856d739564c5c6ce0ecae22
Xiaohan Zhang <hellocharlien@hotmail.com>
2023-07-16 17:53:19 -0700

    Support adding Highlights in UI space

--- Assets/src/controllers/hud/Highlights.cs
+++ Assets/src/controllers/hud/Highlights.cs
@@ -3,6 +3,28 @@ using System.Collections;
 using UnityEngine;
 
 public static class Highlights {
+  public static GameObject CreateUI(GameObject go, Func<bool> destroyWhen = null) {
+    var highlight = PrefabCache.UI.Instantiate("HighlightUI", go.transform);
+
+    GameModelController.main.StartCoroutine(CheckDestroy());
+
+    IEnumerator CheckDestroy() {
+      while (true) {
+        if (go == null) {
+          // don't FadeThenDestroy highlight; highlight is already gone
+          yield break;
+        }
+        if (destroyWhen != null && destroyWhen()) {
+          break;
+        }
+        yield return new WaitForSeconds(0.1f);
+      }
+
+      highlight.AddComponent<FadeThenDestroy>().fadeTime = 0.2f;
+    }
+    return highlight;
+  }
+
   public static GameObject Create(Entity e, Func<bool> destroyWhen = null) {
     var highlight = PrefabCache.UI.Instantiate("Highlight");
 
@@ -14,7 +36,7 @@ public static class Highlights {
         if (highlight == null || e.IsDead || (destroyWhen?.Invoke() ?? false)) {
           break;
         }
-        yield return new WaitForEndOfFrame();
+        yield return new WaitForSeconds(0.1f);
       }
 
       highlight.AddComponent<FadeThenDestroy>().fadeTime = 0.2f;

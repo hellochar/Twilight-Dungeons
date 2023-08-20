100b4dfcdfe98db0de2dc64d9be94e84468b1f8b
Xiaohan Zhang <hellocharlien@hotmail.com>
2023-08-19 20:03:29 -0700

    Add particle effect for when you have mature plants at home

--- Assets/src/controllers/tiles/UpstairsController.cs
+++ Assets/src/controllers/tiles/UpstairsController.cs
@@ -7,9 +7,17 @@ using UnityEngine.EventSystems;
 
 public class UpstairsController : TileController, IOnTopActionHandler {
   public Upstairs upstairs => (Upstairs) tile;
+  public GameObject shiny;
 
   public string OnTopActionName => ((IOnTopActionHandler)upstairs).OnTopActionName;
   public void HandleOnTopAction() {
       ((IOnTopActionHandler)upstairs).HandleOnTopAction();
+  }
+
+  void Update() {
+    bool bHasMaturePlantAtHome = GameModel.main.home.bodies.OfType<Plant>().Any(p => p.IsMature);
+    if (bHasMaturePlantAtHome && !shiny.activeSelf) {
+      shiny.SetActive(true);
+    }
   }
 }

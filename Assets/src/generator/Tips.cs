506d7799c03fefd0f64e9215c10949918fea7dff
Xiaohan Zhang <hellocharlien@hotmail.com>
2023-08-26 15:40:31 -0700

    Re-order tips

--- Assets/src/generator/Tips.cs
+++ Assets/src/generator/Tips.cs
@@ -2,21 +2,21 @@ using System.Collections.Generic;
 
 public static class Tips {
   public static Dictionary<int, string> tipMap = new Dictionary<int, string>() {
-    [0] = "The game auto-saves.\n\nPinch to zoom.\n\nLong tap to inspect a Creature or Grass.",
-    [1] = "Take your time. The game rewards carefully thinking through what will happen each turn.",
-    [2] = "Diagonal movement is just as easy as straight movement.",
-    [3] = "Sometimes, the best move is to wait!",
-    [4] = "The game is supposed to be difficult - don't get discouraged if you die!",
-    [5] = "Regardless of your current depth, taking the Upstairs instantly takes you to the home floor.",
-    [6] = "Jackals alternate between moving 1 and 2 tiles per turn - pay attention to when they move.",
+    // [0] = "The game auto-saves.\n\nPinch to zoom.\n\nLong tap to inspect a Creature or Grass.",
+    // [1] = "Take your time. The game rewards carefully thinking through what will happen each turn.",
+    [2] = "Regardless of your current depth, taking the Upstairs instantly takes you to the home floor.",
+    [3] = "Jackals alternate between moving 1 and 2 tiles per turn - pay attention to when they move.",
+    [4] = "Deduce an enemy's exact HP by keeping track of the total damage dealt to it.",
+    [5] = "Equipping and unequipping items is instant.",
+    // [4] = "The game is supposed to be difficult - don't get discouraged if you die!",
+    [6] = "Diagonal movement is just as easy as straight movement.",
     [7] = "Healing is scarce. Your HP is of utmost importance.",
     [8] = "You found a new Mature plant! Harvest some seeds and re-plant them in the home floor.",
     [9] = "Make full use of Grasses in order to preserve your items!",
-    [10] = "Deduce an enemy's exact HP by keeping track of the total damage dealt to it.",
+    [10] = "Sometimes, the best move is to wait!",
     [11] = "Some enemies are best dealt with using hit-and-dodge tactics, while others must be faced head on!",
     [13] = "Attacking Destructibles (Rubble or Stumps) will not reduce item durability.",
     [14] = "Items that provide Block do not lose durability when blocking 0 damage.",
-    [15] = "Equipping and unequipping items is instant.",
     [16] = "Great job! From here on out the levels get even more exciting. Good luck!"
   };
 }

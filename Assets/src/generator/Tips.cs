389f2a114c7041cc59ff1becf26c46548e7432e9
Xiaohan Zhang <hellocharlien@hotmail.com>
2023-09-30 21:15:54 -0700

    move floor0 tip to floor1; remove the "diagonal movement" tip

--- Assets/src/generator/Tips.cs
+++ Assets/src/generator/Tips.cs
@@ -2,16 +2,15 @@ using System.Collections.Generic;
 
 public static class Tips {
   public static Dictionary<int, string> tipMap = new Dictionary<int, string>() {
-    [0] = "The game auto-saves.\n\nLong tap to inspect a Creature or Grass.",
-    [1] = "Deduce an enemy's exact HP by keeping track of the total damage dealt to it.",
-    [2] = "Regardless of your current depth, taking the Upstairs instantly takes you to the home floor.",
-    [3] = "Jackals alternate between moving 1 and 2 tiles per turn - pay attention to when they move.",
-    [4] = "Equipping and unequipping items is instant.",
-    [5] = "Make full use of Grasses in order to preserve your items!",
-    [6] = "Diagonal movement is just as easy as straight movement.",
+    [1] = "The game auto-saves.\n\nLong tap to inspect a Creature or Grass.",
+    [2] = "Deduce an enemy's HP by keeping track of the total damage dealt to it.",
+    [3] = "Regardless of your current depth, taking the Upstairs instantly takes you to the home floor.",
+    [4] = "Jackals alternate between moving 1 and 2 tiles per turn - pay attention to when they move.",
+    [5] = "Equipping and unequipping items is instant.",
+    [6] = "Make full use of Grasses in order to preserve your items!",
     [7] = "Some enemies are best dealt with using hit-and-dodge tactics, while others must be faced head on!",
     [8] = "You found a new Mature plant! Harvest some seeds and re-plant them in the home floor.",
-    [9] = "Great job! From here on out the levels get even more exciting. Good luck!",
+    [10] = "Great job! From here on out the levels get even more exciting. Good luck!",
   };
 
     // [4] = "The game is supposed to be difficult - don't get discouraged if you die!",

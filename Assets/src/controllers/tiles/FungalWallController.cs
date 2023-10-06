87f97f79a8dfcda691f9051f37e8075773385195
Xiaohan Zhang <hellocharlien@hotmail.com>
2023-10-05 21:25:25 -0700

    Remove extraneous check from FungalWall player interaction

--- Assets/src/controllers/tiles/FungalWallController.cs
+++ Assets/src/controllers/tiles/FungalWallController.cs
@@ -5,12 +5,9 @@ public class FungalWallController : TileController {
   FungalWall fungalWall => (FungalWall) tile;
 
   public override PlayerInteraction GetPlayerInteraction(PointerEventData pointerEventData) {
-    if (tile.visibility != TileVisiblity.Unexplored) {
-      return new SetTasksPlayerInteraction(
-        new MoveToTargetTask(GameModel.main.player, tile.pos),
-        new GenericTask(GameModel.main.player, fungalWall.Clear)
-      );
-    }
-    return base.GetPlayerInteraction(pointerEventData);
+    return new SetTasksPlayerInteraction(
+      new MoveToTargetTask(GameModel.main.player, tile.pos),
+      new GenericTask(GameModel.main.player, fungalWall.Clear)
+    );
   }
 }

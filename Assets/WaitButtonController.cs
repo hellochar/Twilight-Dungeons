e4d1087ed8befac1ab0526439878fbe8b6249caa
Xiaohan Zhang <hellocharlien@hotmail.com>
2023-10-05 20:47:24 -0700

    Fix waitbutton sometimes not showing up

--- Assets/WaitButtonController.cs
+++ Assets/WaitButtonController.cs
@@ -3,20 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WaitButtonController : MonoBehaviour {
-  // Start is called before the first frame update
-  void Start() {
-    Settings.OnChanged += MatchSettings;
-    MatchSettings();
-  }
-
-  void OnDestroy() {
-    Settings.OnChanged -= MatchSettings;
-  }
-
-  private void MatchSettings() {
-    gameObject?.SetActive(!Settings.main.useDPad);
-  }
-
   public void HandleWaitPressed() {
     if (InteractionController.isInputAllowed) {
       GameModel.main.player.SetTasks(new WaitTask(GameModel.main.player, 1));

8d0c298848d2f3a343fff36b49a3b398115b1a17
Xiaohan Zhang <hellocharlien@hotmail.com>
2023-10-22 21:29:33 -0700

    commit sporebloatcontroller

--- Assets/src/controllers/actors/SporeBloatController.cs
+++ Assets/src/controllers/actors/SporeBloatController.cs
@@ -3,8 +3,5 @@ using UnityEngine;
 public class SporeBloatController : ActorController {
   public override void HandleDeath(Entity source) {
     base.HandleDeath(source);
-
-    var explosion = PrefabCache.Effects.Instantiate("SporeBloat Explosion", transform);
-    explosion.transform.parent = null;
   }
 }

9eb9673fce0ba22a93d6a4d5ae6036f57031f7ce
hellochar <hellocharlien@hotmail.com>
2026-03-05 15:04:44 -0800

    github action to deploy on itch.io

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,7 +5,7 @@ import checker from 'vite-plugin-checker'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), checker({ typescript: true })],
-  base: '/Twilight-Dungeons/',
+  base: process.env.VITE_BASE_PATH === '' ? '/' : '/Twilight-Dungeons/',
   esbuild: {
     keepNames: true,
   },
